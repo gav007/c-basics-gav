ff8fa7fa68fda9b0206796a8c4eeb8fe80e2989a
Gav May <gav.s.may@gmail.com>
2025-05-20 00:52:27 +0100

    Variables in C

---
+++ 02-variables/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Gavin";
+    int age = 25;
+    
+    printf("My name is %s.\n" , name);
+    printf("I am %d years old.\n", age);
+    return 0;
+    
+}
