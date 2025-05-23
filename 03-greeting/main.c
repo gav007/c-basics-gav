cfbafbd4794aa6e7f9cd4fe4861d642e08f802c4
Gav May <gav.s.may@gmail.com>
2025-05-23 01:18:52 +0100

    basic name greeting

---
+++ 03-greeting/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    char name[50];
+    int age;
+
+    printf("Enter your name: ");
+    scanf("%49s", name); 
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Hello, %s! You are %d years old.\n", name, age);
+    return 0;   
+}
